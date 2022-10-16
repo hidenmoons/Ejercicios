5e8441b0249362406be9bb9b9e943cff1e43f4c9
Joan Luna <alexis_dx8@hotmail.com>
2022-10-16 13:30:55 -0700

    terminando replace

---
+++ replace_substring/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> words = new List<string>(){"learn", "current", "deal"};
+
+var wordreplace = words.Select(x => x.Contains("ea")? x.Replace("ea","*"):x);
+
+foreach (string word in wordreplace)
+{
+    Console.WriteLine($"{word}");    
+}
+
