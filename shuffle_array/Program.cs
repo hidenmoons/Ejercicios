8f43c4cbb69649d2530a302931bc933fe029340c
Joan Luna <alexis_dx8@hotmail.com>
2022-10-16 13:44:19 -0700

    usar random

---
+++ shuffle_array/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var randeyshon = new Random();
+List<int> array = new List<int>(){1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+var shuffle_array= array.OrderBy(x => randeyshon.Next());
+foreach (var x in shuffle_array){
+Console.WriteLine($"{x}");
+
+}
