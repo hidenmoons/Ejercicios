49ab9af1c7e31e411bdd81181a458c21c88272c2
Joan Luna <alexis_dx8@hotmail.com>
2022-10-16 13:36:24 -0700

    orderby and lastordefault

---
+++ lasword/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> wordlist = new List<string>(){"plane", "ferry", "car", "bike"};
+
+var orderword = wordlist.OrderBy(x =>x).LastOrDefault(x => x.Contains("e"));
+
+
+Console.WriteLine($"{orderword}");
