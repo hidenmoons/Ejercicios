73b1335ce106d5b4ed0118136c9452932d1241e6
Joan Luna <alexis_dx8@hotmail.com>
2022-10-15 20:20:51 -0700

    terminando numeros al cuadrado

--- Squeare_greater_than_20/Program.cs
+++ Squeare_greater_than_20/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+List<int> numbers= new List<int>() {7, 2, 30};
+
+var numbers_square = numbers.Where(x => x*x >20);
+    
+        foreach(var number in numbers_square) { 
+        Console.WriteLine($"{number}-{number*number},");
+        }
+    
+
