b4b8697b34d8d86fb7b56191d5ac4f8e50d6b64b
Joan Luna <alexis_dx8@hotmail.com>
2022-10-15 18:43:07 -0700

    palabras con longitud mayor a 5 y conviertiendolos a mayusculas

---
+++ minimun_length/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> wordlist = new List<string>{"pedro", "juan", "perro", "gato", "celular", "12345"};
+
+
+var upperword = wordlist.Where(x => x.Length>=5).Select(x => x.ToUpper());
+foreach (string word in upperword) {
+
+Console.WriteLine($"{word}, ");
+}
