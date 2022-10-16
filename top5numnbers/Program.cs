2f202f0dd193eac757763828a645fbc62ba2c369
Joan Luna <alexis_dx8@hotmail.com>
2022-10-15 19:54:20 -0700

    terminando top 5

---
+++ top5numnbers/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> top5= new List<int>() {78, -9, 0, 23, 54,  21, 7, 86};  
+
+   var top5numbers = top5.OrderByDescending(x => x).Take(5);
+    foreach(var number in top5numbers) {
+    
+    Console.WriteLine($"{number}");
+    }
