13a15260ca7a27cd6e3d4df1db2779c9fd8326f6
Joan Luna <alexis_dx8@hotmail.com>
2022-10-15 18:43:43 -0700

    terminando numeros

---
+++ number_of_range/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+     List<int> Numbers = new List<int> { 30, 54, 3, 14, 25, 82, 1, 100, 23, 95, 10, 56, 34 };
+
+            var select = Numbers.Where(x => x > 30 && x < 100);
+
+            foreach (var s in select)
+            {
+                Console.Write($"{s} ");
+            }
+        
+
+
+
