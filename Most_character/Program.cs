40f802449894f6fd0c560a06252d8b898ec98076
Joan Luna <alexis_dx8@hotmail.com>
2022-10-16 13:57:42 -0700

    groupby ,count, first

---
+++ Most_character/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string text="n093nfv034nie9";
+
+var counttext= text.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+
+Console.WriteLine(counttext);
